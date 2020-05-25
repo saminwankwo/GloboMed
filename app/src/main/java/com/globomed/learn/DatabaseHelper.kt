f9ac77cf071a944cc576d8f4c67015829dbf7d3c
Nwankwo Samuel <nwankwosami@gmail.com>
2020-05-25 02:48:51 +0100

    started new kotlin project to add sqlite

---
+++ app/src/main/java/com/globomed/learn/DatabaseHelper.kt
@@ -0,0 +1,4 @@
+package com.globomed.learn
+
+class DatabaseHelper : SQLiteOpenHelper {
+}
---
+++ app/src/main/java/com/globomed/learn/Employee.kt
@@ -0,0 +1,8 @@
+package com.globomed.learn
+
+class Employee (
+    val id: Int,
+    val name: String,
+    val dob: Long,
+    val designation: String
+)
